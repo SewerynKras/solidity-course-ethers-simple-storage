7c340a3ce2b06c880d841c0d3b15486cc31cc391
Seweryn Kras <sewerynkras@gmail.com>
2022-07-27 23:55:34 +0200

    add support for goerli network

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,21 @@ dotenv.config();
 import { BigNumber, ethers } from "ethers";
 import fs from "fs";
 
-if (!process.env.PRIVATE_KEY) {
-  throw new Error("No private key in env");
+const providerUrl =
+  process.env.NETWORK === "GOERLI"
+    ? process.env.GOERLI_PROVIDER_URL
+    : process.env.LOCAL_PROVIDER_URL;
+const privateKey =
+  process.env.NETWORK === "GOERLI"
+    ? process.env.GOERLI_PRIVATE_KEY
+    : process.env.LOCAL_PRIVATE_KEY;
+
+if (!privateKey || !providerUrl) {
+  throw new Error("Private key and/or provider url missing");
 }
 
-const provider = new ethers.providers.JsonRpcProvider("http://0.0.0.0:8545");
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+const wallet = new ethers.Wallet(privateKey, provider);
 
 const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi");
 const bin = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin");
@@ -19,7 +28,9 @@ const simpleStorageFactory = new ethers.ContractFactory(
   bin.toString(),
   wallet
 );
-const contract = await simpleStorageFactory.deploy({});
-await contract.store(BigNumber.from("1337"));
+const contract = await simpleStorageFactory.deploy();
+console.log(`Deployed contract @ ${contract.address}`);
+const storeTransaction = await contract.store(BigNumber.from("1337"));
+await storeTransaction.wait();
 const favNum = await contract.retrieve();
 console.log({ raw: favNum, parsed: favNum.toString() });
